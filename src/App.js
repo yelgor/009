495c59f75422421c61fcb87e7775c89485e9d749
Liza <buhir.pn@ucu.edu.ua>
2026-01-30 20:08:58 +0200

    added pages of sign up and login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import Home from "./pages/Home/Home";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
-export default function App() {
-  return <Home />;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
